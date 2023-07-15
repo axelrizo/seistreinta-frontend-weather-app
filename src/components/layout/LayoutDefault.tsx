a28cfd7ab2f70738eb5c671c81fffb9595f21113
AxelRSa <axelg.rizos@gmail.com>
2023-07-15 17:51:41 -0500

    build(font): install monstserrat font

--- src/components/layout/LayoutDefault.tsx
+++ src/components/layout/LayoutDefault.tsx
@@ -1,6 +1,12 @@
 import React, { FC } from 'react'
 import { LayoutFooter } from './LayoutFooter'
 import { LayoutHeader } from './LayoutHeader'
+import { Montserrat } from 'next/font/google'
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-montserrat',
+})
 
 interface Props {
   children: React.ReactNode
@@ -8,7 +14,7 @@ interface Props {
 
 export const LayoutDefault: FC<Props> = ({ children }) => {
   return (
-    <div>
+    <div className={montserrat.variable}>
       <LayoutHeader />
       {children}
       <LayoutFooter />
