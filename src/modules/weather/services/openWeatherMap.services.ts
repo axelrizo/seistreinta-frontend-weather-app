1354e6f24bdf60db303eca9224915cff8cc419a8
AxelRSa <axelg.rizos@gmail.com>
2023-07-16 14:59:37 -0500

    feat(city): add max city limit

--- src/modules/weather/services/openWeatherMap.services.ts
+++ src/modules/weather/services/openWeatherMap.services.ts
@@ -3,8 +3,9 @@ import { openWeatherMapBaseAxiosInstance } from './openWeatherMap.instances'
 
 export const geocodingService = {
   async getCities({ cityName }: { cityName: string }) {
+    const MAX_ALLOWED_CITIES_API_LIMIT = 5
     const response = await openWeatherMapBaseAxiosInstance<CityGeocodeInformation[]>('/geo/1.0/direct', {
-      params: { q: cityName },
+      params: { q: cityName, limit: MAX_ALLOWED_CITIES_API_LIMIT },
     })
     return response.data
   },
