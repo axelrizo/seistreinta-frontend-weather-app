a80f339bad0621a74392465138af7321b6e74350
AxelRSa <axelg.rizos@gmail.com>
2023-07-16 22:11:18 -0500

    feat(theme): add local storage support

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 
 interface Props {
   children: React.ReactNode
@@ -14,9 +14,27 @@ export const ThemeContext = React.createContext<Context>({} as Context)
 export const ThemeProvider: FC<Props> = ({ children }) => {
   const [isDarkThemeActive, setIsDarkThemeActive] = useState(false)
 
-  const toggleTheme = () => {
-    setIsDarkThemeActive((value) => !value)
+  const setLocalStorage = (isDarkThemeActive: boolean) => {
+    localStorage.setItem('theme', JSON.stringify(isDarkThemeActive))
   }
+
+  const readLocalStorage = () => {
+    const theme = localStorage.getItem('theme')
+    if (!theme) return
+    const themeParsed = JSON.parse(theme)
+    setIsDarkThemeActive(themeParsed)
+  }
+
+  const toggleTheme = () => {
+    setIsDarkThemeActive((value) => {
+      setLocalStorage(!value)
+      return !value
+    })
+  }
+
+  useEffect(() => {
+    readLocalStorage()
+  }, [])
 
   return <ThemeContext.Provider value={{ isDarkThemeActive, toggleTheme }}>{children}</ThemeContext.Provider>
 }
