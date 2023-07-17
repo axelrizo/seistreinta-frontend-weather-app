926bfe70e93d6286aa169046cd82e8eec128e6ae
AxelRSa <axelg.rizos@gmail.com>
2023-07-16 19:50:04 -0500

    feat(searchbar): end search bar

--- src/modules/cities/components/CitiesListItem.tsx
+++ src/modules/cities/components/CitiesListItem.tsx
@@ -1,17 +1,26 @@
 import React, { FC } from 'react'
 import { CityInformation } from '../interfaces/CitiesInformation'
 import { useCountryEmoji } from '@/hooks/useCountryEmoji'
-
+import style from './CitiesListItem.module.css'
+import { useRouter } from 'next/router'
+import { FiArrowUpRight } from 'react-icons/fi'
 interface Props {
   city: CityInformation
 }
 
 export const CitiesListItem: FC<Props> = ({ city }) => {
-  const { emoji } = useCountryEmoji({ countryCode: city.country })
+  const { locale } = useRouter()
+  const cityName = city.local_names && locale && city.local_names[locale] ? city.local_names[locale] : city.name
 
   return (
-    <div>
-      {city.country} - <span data-flag={emoji}>{emoji}</span> - {city.state} - {city.name}
+    <div className={`${style['list-item']}`}>
+      <div className={`${style['list_item__city']}`}>
+        <img src={`https://flagcdn.com/48x36/${city.country.toLocaleLowerCase()}.png`} />
+        {city.state ? `${city.state} - ` : ''} {cityName}
+      </div>
+      <div>
+        <FiArrowUpRight size={30} />
+      </div>
     </div>
   )
 }
