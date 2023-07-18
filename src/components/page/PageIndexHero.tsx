72241c1f6bb5bd2f91be62a2874ebe95f8abbf2b
AxelRSa <axelg.rizos@gmail.com>
2023-07-17 20:06:19 -0500

    feat(index): add hero in index page

--- src/components/page/PageIndexHero.tsx
+++ src/components/page/PageIndexHero.tsx
@@ -1,18 +1,59 @@
 import { SearchInformationContext } from '@/context/SearchInformationContext'
-import React, { useContext } from 'react'
+import { CurrentWeather } from '@/modules/weather/interfaces/CurrentWeather'
+import React, { FC, useContext } from 'react'
 import Skeleton from 'react-loading-skeleton'
+import style from './PageIndexHero.module.css'
 
-export const PageIndexHero = () => {
+interface Props {
+  weather: CurrentWeather
+}
+
+export const PageIndexHero: FC<Props> = ({ weather }) => {
+  const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
   const { city } = useContext(SearchInformationContext)
+  const today = new Date()
+  const todayText = weekday[today.getDay()] + ' ' + today.getDate() + ' ' + months[today.getMonth()]
 
   return (
-    <div>
-      <div>
-        <img src={`https://flagcdn.com/48x36/${city!.country.toLocaleLowerCase()}.png`} />
+    <div className={`${style['hero']}`}>
+      <div className={`${style['hero__header']}`}>
+        <img
+          className={`${style['hero__flag']}`}
+          src={`https://flagcdn.com/48x36/${city!.country.toLocaleLowerCase()}.png`}
+        />
         {city!.name}
       </div>
-
-      <Skeleton></Skeleton>
+      <div className={`${style['hero__information']}`}>
+        <img
+          className={`${style['hero__weather-image']}`}
+          src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+          alt=""
+        />
+        <p className={`${style['hero__degrees']}`}>
+          {Math.round(weather.main.temp)}
+          <span className={`${style['hero__degrees-annotation']}`}>&deg;C</span>
+        </p>
+        <p className={`${style['hero__weather']}`}>{weather.weather[0].main}</p>
+        <p>Today · {todayText}</p>
+      </div>
+      {/* <div className={`${style['hero__card']}`}>
+        {weather.main.temp} {weather.weather[0].main} {weather.wind.speed} {weather.main.humidity} {weather.visibility}
+        {weather.main.pressure}
+      </div> */}
     </div>
   )
 }
