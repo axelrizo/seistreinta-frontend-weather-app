a6145d24d78e1195805bb2c28ffd02f844172198
AxelRSa <axelg.rizos@gmail.com>
2023-07-17 20:05:51 -0500

    feat(footer): change footer

--- src/components/layout/LayoutFooter.tsx
+++ src/components/layout/LayoutFooter.tsx
@@ -3,5 +3,9 @@ import { FC } from 'react'
 
 export const LayoutFooter: FC = () => {
   const { t } = useTranslation()
-  return <div style={{ paddingBottom: '70px', display: 'flex', justifyContent: 'center' }}>{t('footer.text')}</div>
+  return (
+    <div style={{ paddingBottom: '70px', display: 'flex', justifyContent: 'center', paddingTop: '16px' }}>
+      {t('footer.text')}
+    </div>
+  )
 }
