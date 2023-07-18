a4c930e07d25e53268df54b4db8562bb38a80819
AxelRSa <axelg.rizos@gmail.com>
2023-07-17 20:44:54 -0500

    feat(forecast): add forecast endpoint

--- src/modules/weather/services/weather.services.ts
+++ src/modules/weather/services/weather.services.ts
@@ -1,10 +1,18 @@
 import { openWeatherMapBaseAxiosInstance } from '../../openWeatherMap/instances/openWeatherMap.instances'
 import { CurrentWeather } from '../interfaces/CurrentWeather'
+import { ForecastWeather } from '../interfaces/ForecastWeather'
 
 export const weatherServices = {
   async getForecast({ lon, lat }: { lon: number; lat: number }) {
     const response = await openWeatherMapBaseAxiosInstance<CurrentWeather>('/data/2.5/weather', {
       params: { lat, lon, units: 'metric' },
+    })
+    return response.data
+  },
+
+  async get5DayForecast({ lat, lon }: { lon: number; lat: number }) {
+    const response = await openWeatherMapBaseAxiosInstance<ForecastWeather>('/data/2.5/forecast', {
+      params: { lat, lon },
     })
     return response.data
   },
